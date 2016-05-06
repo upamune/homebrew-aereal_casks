1c2a12a5a01c6035b540f20073179f60dbc3563c
upamune <jajkeqos@gmail.com>
2016-05-06 18:20:33 +0900

    Upgrade to 4.3.5

--- Casks/aquaskk.rb
+++ Casks/aquaskk.rb
@@ -1,9 +1,9 @@
 cask :v1 => 'aquaskk' do
-  version '4.3.2'
+  version '4.3.5'
   url "https://github.com/codefirst/aquaskk/releases/download/#{version}/AquaSKK-#{version}.dmg"
   name 'AquaSKK'
   homepage 'https://github.com/codefirst/aquaskk'
-  sha256 '1294b4e02b563410796737c86467cd321f87cc85e359a132c26ea0a2f1a344fe'
+  sha256 '111825f13c0757fc98e669ee149bf41d343efedfc9808573f996bf1e5d052309'
   license :gpl # GPL v2
   pkg 'AquaSKK.pkg'
   uninstall :pkgutil => 'jp.sourceforge.inputmethod.aquaskk.pkg'
